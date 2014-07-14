788ea688241212228a1d421d5d1ecde051c4cde0
abinani <abinani@fas.harvard.edu>
2014-07-14 18:58:51 -0400

    First checkin

---
+++ app/views/_master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title',"Developer's Best Friend")</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css" rel="stylesheet">
+    @yield('head')
+
+</head>
+<body role="document" style="padding-top: 20px;">
+    <div class="container" role="main">
+       @yield('content') 
+    </div>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+   @yield('footer') 
+
+</body>
+</html>
---
+++ app/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('_master')
+
+@section('content')
+<div class="jumbotron">
+    <h2><strong>Welcome to Developer's Best Friend </strong></h2>
+    <p> The website houses a variety of utilities that developers can use in their day-to-day work.
+     The current utilities include <em>Lorem Ipsum Generator</em> and a <em>Random User Generator</em>.
+    </p>
+</div>
+@stop
